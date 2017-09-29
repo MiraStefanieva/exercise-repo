5184160cac9ac6f441cad70619c326b7fd100adf
MiraStefanieva <32309459+MiraStefanieva@users.noreply.github.com>
2017-09-29 21:47:08 +0300

    Add files via upload

---
+++ 02. Conditional statements and Loops/02. Choose a drink 2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Choose_a_drink_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string profession = Console.ReadLine();
+            int quantity = int.Parse(Console.ReadLine());
+
+            switch (profession)
+            {
+                case "Athlete": Console.WriteLine($"The {profession} has to pay {(0.70 * quantity):F2}."); break;
+                case "Businessman": 
+                case "Businesswoman": Console.WriteLine($"The {profession} has to pay {(1.00 * quantity):F2}."); break;
+                case "SoftUni Student": Console.WriteLine($"The {profession} has to pay {(1.70 * quantity):F2}."); break;
+                default: Console.WriteLine($"The {profession} has to pay {(1.20 * quantity):F2}."); break;
+
+            }
+        }
+    }
+}
