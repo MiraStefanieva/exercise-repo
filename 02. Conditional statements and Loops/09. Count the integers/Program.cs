53f0c21fde46178a3c81ceb2660a98105e7f753d
MiraStefanieva <32309459+MiraStefanieva@users.noreply.github.com>
2017-09-29 21:57:15 +0300

    Add files via upload

---
+++ 02. Conditional statements and Loops/09. Count the integers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_Count_the_Integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var integerCount = 0;
+            try
+            {
+                while (true)
+                {
+                    var input = int.Parse(Console.ReadLine());
+                    integerCount++;
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine(integerCount);
+            }
+        }
+    }
+}
