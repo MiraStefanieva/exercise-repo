9201dc6567fd0147952faa41bf31b964bb207dfa
MiraStefanieva <32309459+MiraStefanieva@users.noreply.github.com>
2017-09-29 22:02:24 +0300

    Add files via upload

---
+++ 02. Conditional statements and Loops/13. Game of Numbers/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_Game_of_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int magicalNumber = int.Parse(Console.ReadLine());
+            int counter = 0;
+            int matchCounter = 0;
+            for (int i = secondNumber; i >= firstNumber; i--)
+            {
+                for (int j = secondNumber; j >= firstNumber; j--)
+                {
+                    counter++;
+                    if (i + j == magicalNumber)
+                    {
+                        Console.WriteLine($"Number found! {i} + {j} = {magicalNumber}");
+                        matchCounter++;
+                        return;
+                    }
+                    
+                }
+            }
+            if (matchCounter == 0)
+            {
+                Console.WriteLine($"{counter} combinations - neither equals {magicalNumber}");
+            }
+           
+        }
+    }
+}
