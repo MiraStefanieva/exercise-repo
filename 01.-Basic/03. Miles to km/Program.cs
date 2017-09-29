dc5764e65b9127f6d6725828114430bb7c018f53
MiraStefanieva <32309459+MiraStefanieva@users.noreply.github.com>
2017-09-29 21:34:16 +0300

    Add files via upload

---
+++ 01.-Basic/03. Miles to km/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miles_to_Kilometers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var mile = 1.60934;
+            var input = Double.Parse(Console.ReadLine());
+            var output = input * mile;
+            Console.WriteLine($"{output:F2}");
+        }
+    }
+}
