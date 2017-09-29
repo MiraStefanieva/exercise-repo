7912188646a6f83d23cbf7997898d5cf77d483a0
MiraStefanieva <32309459+MiraStefanieva@users.noreply.github.com>
2017-09-29 21:54:22 +0300

    Add files via upload

---
+++ 02. Conditional statements and Loops/07. Cake ingredients/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_Cake_Ingredients
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            int ingredientsNumber = 0;
+            while( input != "Bake!")
+            {
+                Console.WriteLine($"Adding ingredient {input}.");
+                input = Console.ReadLine();
+                ingredientsNumber++;
+            }
+            Console.WriteLine($"Preparing cake with {ingredientsNumber} ingredients.");
+        }
+    }
+}
