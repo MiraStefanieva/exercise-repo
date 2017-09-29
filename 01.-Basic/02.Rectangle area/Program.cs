488389de33e8bbd9bb92b53c00369481e029c07f
MiraStefanieva <32309459+MiraStefanieva@users.noreply.github.com>
2017-09-29 21:32:02 +0300

    Add files via upload

---
+++ 01.-Basic/02.Rectangle area/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rectangle_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var width = double.Parse(Console.ReadLine());
+            var heighth = double.Parse(Console.ReadLine());
+            var area = width * heighth;
+            Console.WriteLine($"{area:F2}");
+
+        }
+    }
+}
