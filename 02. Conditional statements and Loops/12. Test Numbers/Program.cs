149b52bfc9dec9c360d44b89c45f6e6e21089ed3
MiraStefanieva <32309459+MiraStefanieva@users.noreply.github.com>
2017-09-29 22:01:20 +0300

    Add files via upload

---
+++ 02. Conditional statements and Loops/12. Test Numbers/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_Test_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            int boundary = int.Parse(Console.ReadLine());
+            var combinations = 0;
+            var sum = 0;
+
+            for (int i = n; i >= 1; i--)
+            {
+                for (int j = 1; j <= m; j++)
+                {
+                    sum += 3 * i * j;
+                    combinations++;
+                    if (sum >= boundary)
+                    {
+                        Console.WriteLine($"{combinations} combinations");
+                        Console.WriteLine($"Sum: {sum} >= {boundary}");
+                        return;
+                    }
+
+                }
+            }
+            
+           
+                Console.WriteLine($"{combinations} combinations");
+                Console.WriteLine($"Sum: {sum}");
+            
+            
+            
+
+        }
+    }
+}
